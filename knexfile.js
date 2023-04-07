b9278235a00dac8c7effe98c8099f0eb806faf33
Scott Knell <104751509+sc124578@users.noreply.github.com>
2023-04-07 11:07:35 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://hwbiptzq:NsgzYeFK75no0SPY9z9qzsLdVRH-PaTx@heffalump.db.elephantsql.com/hwbiptzq"
 
 
 module.exports = {
